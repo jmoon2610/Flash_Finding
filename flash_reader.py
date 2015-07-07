192438093083ea7e6b493014fbf878a846ea8435
jmoon2610 <jarrettmoon@yahoo.com>
2015-07-06 20:04:46 -0500

    7/6/15 update

--- flash_reader.py
+++ flash_reader.py
@@ -4,71 +4,90 @@ import math
 import array
 import sys
 
-#Simple function returns basic info about waveform peak, amplitude, 
-#point of peak, and how many times the peak value occurs as a potential check
-#for pseudo-peaks. Returns relative values, no correction for actual window 
-#start time
-def peak_info(waveform):
-
-    amplitude = float(max(waveform))
-    amplitude_t = waveform.index(max(waveform))
-    peak_count = waveform.count(amplitude)
-
-    return amplitude, amplitude_t, peak_count
-
-
-#Returns the average baseline value and baseline standard deviation, given 
-#start and end points
-def get_baseline_info(waveform,base_start,base_end):
-
-    base_length = base_end-base_start+1
-    baseline = waveform[base_start:(base_end+1)]
-    base_avg = float(sum(baseline))/float(base_length)
-
-    base_stdev = 0
-    for x in baseline:
-        base_stdev = (x - base_avg)**2
-    base_stdev = math.sqrt(base_stdev/base_length)
-
-    return base_avg,base_stdev
-
-
-#Returns the total integrated charge in a waveform, then subtracts a 
-#constant baseline. NOT CURRENTLY CALIBRATED
-def get_charge(waveform,base_start,base_end):
-
-    (base_avg,base_stdev) = get_baseline_info(waveform,base_start,base_end)
-
-    charge = 0
-
-    for x in waveform[base_end:]:
-        charge = charge + x
-
-    charge = float(charge - base_avg*(len(waveform)-base_end))
-
-    return charge
-
-
-#Searches through the list of TTL_pulse times, and then for another 
-#given pulse, returns the distance to the most recent TTL pulse.
-#Returns a very large value (10000) if no recent pulse is found.
-#Note it finds the nearest past pulse only
-def nearest_TTL(TTL_list,pulse_time):
+#Function takes a waveform with multiple TTL pulses, scans through looking
+#for peaks, converts the frame position to a global TTL peak time in nanoseconds.
+#Function assumes that all peaks are nearly the same height in its computation
+def TTL_info(waveform,frame_start_time):
     
-    time_diff_list = []
+    maximum = float(max(waveform))
+    
+    peak_number = 0
+    peak_position = []
 
-    for x in TTL_list:
+    for bin in range(len(waveform)):
 
-        time_diff_list.append(pulse_time -x)
+        if (waveform[bin] > waveform[bin -10] and waveform[bin] > waveform[bin+10] and waveform[bin]/maximum > 0.95):
+            position = bin + waveform[(bin-10):(bin+10)].index(max(waveform[(bin-10):(bin+10)]))
+            peak_position.append(position*15.625+frame_start_time)
 
-    min = 100000                     #Some obvious default value of no nearest pulse found
+    peak_position = list(set(peak_position))
+    peak_position.sort()
+    peak_number = len(peak_position)
 
-    for x in time_diff_list:
+    return peak_number,peak_position 
 
-        if (x<min and x >0):     
-            min = x                  
 
-    return min
+
+#Takes a waveform and tries to establish a baseline by starting at the beginning
+#of the waveform and walking through different baseline sizes to see which minimizes
+#the baseline RMS
+def get_baseline_info(waveform):
+
+    baseline_start = 0
+    baseline_std = 100000
+
+    for baseline_end in [500,450,400,350,300,250,200,150,125,100,75,50,25]:
+    
+        trial_baseline_std = 0
+        baseline_mean = float(sum(waveform[baseline_start:baseline_end]))/len(waveform[baseline_start:baseline_end])
+
+        for x in waveform[baseline_start:baseline_end]:
+            trial_baseline_std  = trial_baseline_std + (x - baseline_mean)**2
+
+        trial_baseline_std = math.sqrt(trial_baseline_std)/float(math.sqrt(len(waveform[baseline_start:baseline_end])))
+
+
+        if trial_baseline_std < baseline_std:
+
+            baseline_std = trial_baseline_std
+
+    return baseline_mean,baseline_std
+    
+
+#Reads in a waveform, and the list of TTL pulse real times, as well as the 
+#real time at which this waveform starts. It the looks at every TTL time, opens a
+#window of predetermined size, and then integrates the corresponding time window
+#in the waveform, subtracts a pedestal and stores each integral separately
+# in an output list.
+def get_fixed_window_charge(waveform,TTL_list,waveform_start_t):
+
+    window_width = 300                   #integration window width in ns
+
+    charge_in_window = []
+
+    (baseline_mean,baseline_std) = get_baseline_info(waveform)
+
+    for TTL_start in TTL_list:
+        if TTL_start + window_width > waveform_start_t + len(waveform)*15.625:     #prevents a window which will get cut off
+            break
+
+        window_start = int(round((TTL_start - waveform_start_t)/15.625))
+        window_end = window_start +int(round(window_width/15.625))
+
+        
+        if len(waveform[window_start:window_end]) > 0:
+            
+            charge = 0
+            for x in waveform[window_start:window_end]:
+                charge = charge + x
+    
+            charge = charge - (window_end-window_start)*baseline_mean
+
+            charge_in_window.append(charge)
+
+ 
+    return charge_in_window
+
 
 input_file_name = sys.argv[1]
 output_file_name = sys.argv[2]
@@ -77,92 +96,70 @@ output_file = ROOT.TFile(output_file_name,'RECREATE')
 input_tree = input_file.Get('rawdigitwriter/RawData/OpDetWaveforms')
 output_tree = ROOT.TTree('output_tree','Flasher Data Output Tree')
 
-event_id = array.array('I',[0])
+event_number = array.array('I',[0])
 baseline_average = array.array('d',[0])
 baseline_std = array.array('d',[0])
-peak_amplitude = array.array('d',[0])
-real_peak_time = array.array('d',[0])
-frame_peak_time = array.array('I',[0])
-number_of_maxima = array.array('I',[0])
-total_charge = array.array('d',[0])
-t_to_last_TTL = array.array('d',[0])
+fixed_window_charge = array.array('d',[0]*23)
 readout_channel = array.array('I',[0])
-is_cutoff = array.array('d',[0])
 
-output_tree.Branch('event_id',event_id,'event_id/I')
+output_tree.Branch('event_number',event_number,'event_number/I')
 output_tree.Branch('baseline_average',baseline_average,'baseline_average/D')
 output_tree.Branch('baseline_std',baseline_std,'baseline_std/D')
-output_tree.Branch('peak_amplitude',peak_amplitude,'peak_amplitude/D')
-output_tree.Branch('real_peak_time',real_peak_time,'real_peak_time/D')
-output_tree.Branch('frame_peak_time',frame_peak_time,'frame_peak_time/I')
-output_tree.Branch('number_of_maxima',number_of_maxima,'number_of_maxima/I')
-output_tree.Branch('total_charge',total_charge,'total_charge/D')
-output_tree.Branch('t_to_last_TTL',t_to_last_TTL,'t_to_last_TTL/D')
+output_tree.Branch('fixed_window_charge',fixed_window_charge,'fixed_window_charge[23]/D')
 output_tree.Branch('readout_channel',readout_channel,'readout_channel/I')
-output_tree.Branch('is_cutoff',is_cutoff,'is_cutoff/D')
 
-window_start_times = []
+event_start_times = []
 
-#This first loop simply finds TTL pulses, i.e. those from readoudch (mod 37) =0
-#then fills a list with the real pulse times
-
-#COULD BE MORE EFFICIENT,SEVERAL REDUNDANT CALCULATIONS.  HAVEN'T BOTHERED TO FIX YET. 
 for entry in input_tree:     
 
     waveform = []
-#    if ((entry.readoutch) % 37 == 0 and entry.readoutch != 0):
-    if (entry.adcs.size() == 20):
-       
+    if (entry.readoutch == 37):
+
         for x in range(entry.adcs.size()):
             waveform.append(entry.adcs[x])
+            
+        waveform_start_time = entry.timestamp
+        num_peaks,TTL_peak_times = TTL_info(waveform,waveform_start_time)
 
-        (amplitude, amplitude_t, peak_count) = peak_info(waveform)
-        start_time = entry.timestamp + amplitude_t
-        window_start_times.append(start_time)
-
-print len(window_start_times)
+        event_start_times.append(TTL_peak_times)
+    
+#At this point, event_start_times is a list of lists. Each list in the list
+#corresponds to the TTL start times found for an individual event
 
 tick = 0
-for entry in input_tree: 
-    if((tick % 1000) ==0):
-        print tick
-    tick = tick +1
+for entry in input_tree:
     
-    baseline_start = 10             #User determined beginning of signal free region
-    baseline_end = 100              #User determined end of signal free region
-
-    waveform = []
+    if (tick % 10000 == 0):
+        print tick
+    tick = tick + 1
 
     bins = entry.adcs.size()
+    if bins != 1500:
+        continue
+
+    waveform = []
     for x in range(bins):
         waveform.append(entry.adcs[x])
 
-#    if ((entry.readoutch) % 37 !=0):
-    if(entry.adcs.size() == 1500):
+    if (entry.readoutch / 100 < 37):
 
-        (amplitude,amplitude_t,peak_count) = peak_info(waveform)
-        (baseline_avg,baseline_stdev) = get_baseline_info(waveform,baseline_start,baseline_end)
-        charge = get_charge(waveform,baseline_start,baseline_end)       
-        absolute_peak_time = entry.timestamp + amplitude_t
-        time_since_TTL = nearest_TTL(window_start_times,absolute_peak_time)
+        real_waveform_start_t = entry.timestamp
+        index = entry.event 
+
+        (baseline_avg,baseline_stdev) = get_baseline_info(waveform)       
         channel = entry.readoutch
-        index = entry.event
-        final_bin_ratio = sum(waveform[-10:])/(baseline_avg*10.0)
-
-        event_id[0]              = index
+        window_charge = get_fixed_window_charge(waveform,event_start_times[index-1],real_waveform_start_t)
+        
+            
+        event_number[0]          = index
         baseline_average[0]      = baseline_avg
         baseline_std[0]          = baseline_stdev
-        peak_amplitude[0]        = amplitude
-        real_peak_time[0]        = absolute_peak_time
-        frame_peak_time[0]       = amplitude_t
-        number_of_maxima[0]      = peak_count
-        total_charge[0]          = charge
-        t_to_last_TTL[0]         = time_since_TTL
         readout_channel[0]       = channel
-        is_cutoff[0]             = final_bin_ratio
 
-        index = index + 1
+        for x in range(len(window_charge)):
+            fixed_window_charge[x]   = window_charge[x]
 
+        
         output_tree.Fill()
 
 output_file.Write()
